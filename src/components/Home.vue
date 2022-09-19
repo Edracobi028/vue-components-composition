518ff0f3f63ff015840bb7df5a88a4a52fa7f755
Eduardo Razo <esse_rios_e@hotmail.com>
2022-09-18 21:51:49 -0500

    Computed 16/23

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>Hola</div>
+    <div>fullName: {{ fullName }}</div>
 </template>
 
 <script>
-    import { onMounted } from "vue";
+import { ref, computed } from 'vue';
+
     export default{
-        setup(){
-            onMounted(()=>{
-              console.log("mounted!");  
+        setup() {
+            const firstName = ref("Eduardo");
+            const lastName = ref("Razo");
+
+            const fullName = computed(() => {
+                return `${firstName.value} ${lastName.value} `;
             });
+
+
+            return{
+                fullName,
+            };
         },
     };
 </script>
